e5322dba8cbbe01ad7dcd375642d789cd792d6f4
Vincent Tollu <vincent.tollu@orange.com>
2017-04-06 14:21:17 +0200

    service pipes

---
+++ FabrikamFiber/FFServices/ServiceWeather.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFServices
+{
+    public class ServiceWeather : IServiceWeather
+    {
+        public string GetWeather(string cityName)
+        {
+            return "toto";
+        }
+    }
+}
--- FabrikamFiber/FabrikamFiber.Web/Controllers/HomeController.cs
+++ FabrikamFiber/FabrikamFiber.Web/Controllers/HomeController.cs
@@ -9,21 +9,17 @@
     using System;
     using System.Reflection;
     using System.IO;
+    using FFServices;
 
     public class HomeController : Controller
     {
-        private readonly IServiceTicketRepository serviceTickets;
-        private readonly IMessageRepository messageRepository;
-        private readonly IAlertRepository alertRepository;
-        private readonly IScheduleItemRepository scheduleItemRepository;
+        private readonly IServiceWeather serviceWeather;
+      
 
         public HomeController(
-                              IServiceTicketRepository serviceTickets,
-                              IMessageRepository messageRepository,
-                              IAlertRepository alertRepository,
-                              IScheduleItemRepository scheduleItemRepository)
+                              IServiceWeather serviceWeather)
         {
-         
+            this.serviceWeather = serviceWeather;
         }
 
         public ActionResult Index()
@@ -38,9 +34,14 @@
 
             ViewBag.BuildDate = RetrieveLinkerTimestamp();
             ViewBag.Version = GetAssemblyVersion();
-            ViewBag.Meteo = "Sunny";
+            ViewBag.Meteo = serviceWeather.GetWeather("Nantes");
              
             return View(viewModel);
+        }
+
+        private dynamic GetWeather()
+        {
+            throw new NotImplementedException();
         }
 
         private dynamic GetAssemblyVersion()
