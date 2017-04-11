d3981d08b10458e530bced989fb1511ab29ed757
Vincent Tollu <vincent.tollu@orange.com>
2017-04-11 19:43:58 +0200

    true service class

--- FabrikamFiber/FabrikamFiber.Web/App_Start/NinjectMVC3.cs
+++ FabrikamFiber/FabrikamFiber.Web/App_Start/NinjectMVC3.cs
@@ -48,7 +48,7 @@ namespace FabrikamFiber.Web.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
-            kernel.Bind<IServiceWeather>().To<ServiceWeatherProxyMock>();
+            kernel.Bind<IServiceWeather>().To<ServiceWeather>();
         }
     }
 }
